f365cdef12a2ced15a1c5fe9b5be56893e008dc7
EhsanRP <sir.ehrp@gmail.com>
2020-12-02 01:09:35 +0330

    Image System Works

--- src/main/java/guru/springframework/controllers/ImageController.java
+++ src/main/java/guru/springframework/controllers/ImageController.java
@@ -1,11 +1,21 @@
 package guru.springframework.controllers;
 
+import guru.springframework.commands.RecipeCommand;
 import guru.springframework.services.ImageService;
 import guru.springframework.services.RecipeService;
+import org.apache.tomcat.util.http.fileupload.IOUtils;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.multipart.MultipartFile;
+
+import javax.servlet.http.HttpServletResponse;
+import java.io.ByteArrayInputStream;
+import java.io.IOException;
+import java.io.InputStream;
 
 @Controller
 public class ImageController {
@@ -29,5 +39,21 @@ public class ImageController {
     public String handleImagePost(@PathVariable String recipeId, @RequestParam("imagefile") MultipartFile file){
         imageService.saveImageFile(Long.valueOf(recipeId),file);
         return "redirect:/recipe/"+recipeId+"/show";
+    }
+
+    @GetMapping("recipe/{recipeId}/recipeimage")
+    public void renderImageFromDb(@PathVariable String recipeId, HttpServletResponse response) throws IOException {
+        RecipeCommand recipeCommand = recipeService.findCommandById(Long.valueOf(recipeId));
+
+        byte[] byteArray = new byte[recipeCommand.getImage().length];
+
+        int i = 0;
+
+        for (byte wrappedByte: recipeCommand.getImage()) {
+            byteArray[i++] = wrappedByte;
+        }
+        response.setContentType("image/jpeg");
+        InputStream inputStream = new ByteArrayInputStream(byteArray);
+        IOUtils.copy(inputStream,response.getOutputStream());
     }
 }
